30f712e8c5bee745f61044c2a552a3ad044af781
Bulygin Evgeny <tyztuz@gmail.com>
2025-02-14 15:02:10 +0500

    `GZipResponder` has been rewritten so that it no longer throws warnings like `Exception ignored in: <gzip on 0x7f0fd0276380>`

--- tests/middleware/test_gzip.py
+++ tests/middleware/test_gzip.py
@@ -1,9 +1,22 @@
+import gc
+import gzip
+import sys
+from asyncio import CancelledError
+from collections.abc import Iterator
+from contextlib import contextmanager
+from io import BytesIO
+from typing import TYPE_CHECKING, Callable
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
-from starlette.middleware.gzip import GZipMiddleware
+from starlette.middleware.gzip import GZipMiddleware, GZipResponder
 from starlette.requests import Request
 from starlette.responses import ContentStream, PlainTextResponse, StreamingResponse
 from starlette.routing import Route
+from starlette.testclient import TestClient
 from tests.types import TestClientFactory
 
 
@@ -91,7 +104,9 @@ def test_gzip_ignored_for_responses_with_encoding_set(
                 yield bytes
 
         streaming = generator(bytes=b"x" * 400, count=10)
-        return StreamingResponse(streaming, status_code=200, headers={"Content-Encoding": "text"})
+        return StreamingResponse(
+            streaming, status_code=200, headers={"Content-Encoding": "text"}
+        )
 
     app = Starlette(
         routes=[Route("/", endpoint=homepage)],
@@ -104,3 +119,63 @@ def test_gzip_ignored_for_responses_with_encoding_set(
     assert response.text == "x" * 4000
     assert response.headers["Content-Encoding"] == "text"
     assert "Content-Length" not in response.headers
+
+
+if TYPE_CHECKING:
+    from sys import UnraisableHookArgs
+else:
+    UnraisableHookArgs = object
+
+
+@contextmanager
+def override_custom_unraisable_exception_hook_and_gc_collect(
+    unraisable_hook: Callable[[UnraisableHookArgs], None],
+) -> Iterator[None]:
+    unraisable_hook_before_test = sys.unraisablehook
+    sys.unraisablehook = unraisable_hook
+    try:
+        yield
+        gc.collect()
+    finally:
+        sys.unraisablehook = unraisable_hook_before_test
+
+
+# https://github.com/python/cpython/blob/247b50dec8af47ed8a80069117e07b7139f9d54f/Doc/whatsnew/3.13.rst#io
+@pytest.mark.skipif(sys.version_info < (3, 13), reason="requires python3.13 or higher")
+def test_gzip_responder_not_writes_not_raised_exception_warning(
+    test_client_factory: TestClientFactory,
+) -> None:
+    # check unraisablehook trgiggers when BytesIO destroyed before GzipFile
+    mock_for_test_bytesio_and_gzip_unraisable = Mock()
+    with override_custom_unraisable_exception_hook_and_gc_collect(
+        unraisable_hook=mock_for_test_bytesio_and_gzip_unraisable
+    ):
+        b = BytesIO()
+        g = gzip.GzipFile(fileobj=b, mode="wb")
+        del b
+        del g
+
+    assert mock_for_test_bytesio_and_gzip_unraisable.called
+
+    mock_for_test_gzip_responder_unraisable = Mock()
+    with override_custom_unraisable_exception_hook_and_gc_collect(
+        unraisable_hook=mock_for_test_gzip_responder_unraisable
+    ):
+
+        async def homepage(request: Request) -> PlainTextResponse:
+            return PlainTextResponse("Hello, world" * 100)
+
+        def gzip_responder_call_with_cancelled_error(*args, **kwargs):
+            raise CancelledError()
+
+        GZipResponder.__call__ = gzip_responder_call_with_cancelled_error
+
+        app = Starlette(
+            routes=[Route("/", homepage)], middleware=[Middleware(GZipMiddleware)]
+        )
+
+        test_client: TestClient = TestClient(app, raise_server_exceptions=False)
+
+        test_client.get("/", headers={"accept-encoding": "gzip"})
+
+    assert not mock_for_test_gzip_responder_unraisable.called
